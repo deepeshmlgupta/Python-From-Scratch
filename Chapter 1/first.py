5a905c5289d8c3279372a0abba0510ab6789960d
deepeshgupta3257 <108192513+deepeshmlgupta@users.noreply.github.com>
2024-09-02 23:23:46 +0530

    first code added

---
+++ Chapter 1/first.py
@@ -0,0 +1,12 @@
+# import pyjokes
+
+# joke=pyjokes.get_joke()
+# print(joke)
+
+# print("hello")
+
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hi My name is Deepesh")
+engine.runAndWait()
