d033d25e52dff242d1626ad5e0123620f40339d5
deepeshgupta3257 <108192513+deepeshmlgupta@users.noreply.github.com>
2024-09-04 23:49:32 +0530

    code added

---
+++ Chapter 1/conditional.py
@@ -0,0 +1,13 @@
+"""
+Python if...elif...else Statement
+The if...elif...else statement is used in Python for decision making.
+"""
+
+day = input("Enter the day of the week").lower()
+
+if day == "saturday" or day == "sunday":
+    print("I will be learning")
+else:
+    print("I will be doing practice")
+
+
